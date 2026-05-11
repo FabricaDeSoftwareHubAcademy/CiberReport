cff917e57378ce2df2f3b151833ce49f4a8bbb22
Rafael Rotela <rjvrafael22@gmail.com>
2026-05-11 01:19:14 -0400

    style(menu-lateral/superior) estilizaçao e adicao de submenu interativo

--- JAVASCRIPT/menu.js
+++ JAVASCRIPT/menu.js
@@ -4,3 +4,8 @@ document.getElementById('open_btn').addEventListener('click', function() {
     sideBar.classList.toggle('open-sidebar');
 });
 
+const submenu = document.getElementById('submenu');
+
+document.getElementById('dropdown').addEventListener('click', function(){
+    submenu.classList.toggle('open_submenu');
+})
