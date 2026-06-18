2c6c29df01d80a624ea5a8e455f7e062073aaad3
Izadora45 <izadoraaparecidasilvapereira@gmail.com>
2026-06-18 06:48:45 -0400

    style(clientes):ajuste editar.php

--- Views/editar.php
+++ Views/editar.php
@@ -51,25 +51,188 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EDITAR Empresa</title>
+    <link rel="stylesheet" href="../assets/CSS/Componentes/menu-superior.css">
+    <link rel="stylesheet" href="../assets/CSS/Componentes/menu-lateral.css">
+    <link rel="stylesheet" href="../assets/CSS/style.css">
+    <link rel="stylesheet" href="../assets/CSS/Componentes/button.css">
+    <link rel="stylesheet" href="../assets/CSS/Componentes/tabela.css">
+    <link rel="stylesheet" href="../assets/CSS/Pages/clientes.css">
 </head>
 <body>
     <h2 class="titulo-pagina">EDITAR EMPRESA</h2>
-    <form method="post">
-        <input type="text" name="nome_fantasia" value="<?php echo $dados_empresa['nome_fantasia'];?>">
-        <input type="text" name="razao_social" value="<?php echo $dados_empresa['razao_social'];?>">
-        <input type="tel" name="telefone" value="<?php echo $dados_empresa['telefone'];?>">
-        <input type="email" name="email_contato" value="<?php echo $dados_empresa['email_contato'];?>">
-        <input type="text" name="cnpj" value="<?php echo $dados_empresa['cnpj'];?>">
-        <input type="text" name="cep" value="<?php echo $dados_endereco['cep'];?>">
-        <input type="text" name="rua" value="<?php echo $dados_endereco['rua'];?>">
-        <input type="text" name="numero" value="<?php echo $dados_endereco['numero'];?>">
-        <input type="text" name="complemento" value="<?php echo $dados_endereco['complemento'];?>">
-        <input type="text" name="bairro" value="<?php echo $dados_endereco['bairro'];?>">
-        <input type="text" name="cidade" value="<?php echo $dados_endereco['cidade'];?>">
-        <input type="text" name="estado" value="<?php echo $dados_endereco['estado'];?>">
-        <input type="text" name="pais" value="<?php echo $dados_endereco['pais'];?>">
-        <input type="text" name="responsavel" value="<?php echo $dados_empresa['responsavel'];?>">
-        <button type="submit" class="modal-clientes-btn-salvar">SALVAR</button>
+    <form method="post" class="modal-clientes-div-form">
+
+        <div class="modal-clientes-corpo-form">
+
+            <div class="modal-clientes-secao-label">
+                <i class="fa-solid fa-file-lines"></i>
+                <h3>Dados da Empresa</h3>
+            </div>
+
+            <div class="modal-clientes-form-row">
+
+                <div class="modal-clientes-area-dados-cliente">
+                    <label>Nome da Empresa</label>
+                    <input
+                        type="text"
+                        name="nome_fantasia"
+                        value="<?php echo $dados_empresa['nome_fantasia']; ?>"
+                    />
+                </div>
+
+                <div class="modal-clientes-area-dados-cliente">
+                    <label>Razão Social</label>
+                    <input
+                        type="text"
+                        name="razao_social"
+                        value="<?php echo $dados_empresa['razao_social']; ?>"
+                    />
+                </div>
+
+                <div class="modal-clientes-area-dados-cliente-tel">
+                    <label>Telefone</label>
+                    <div class="modal-clientes-campo-telefone">
+                        <i class="fa fa-phone"></i>
+                        <input
+                            type="text"
+                            name="telefone"
+                            value="<?php echo $dados_empresa['telefone']; ?>"
+                        />
+                    </div>
+                </div>
+
+                <div class="modal-clientes-area-dados-cliente-email">
+                    <label>E-mail</label>
+                    <div class="modal-clientes-campo-email">
+                        <input
+                            type="email"
+                            name="email_contato"
+                            value="<?php echo $dados_empresa['email_contato']; ?>"
+                        />
+                    </div>
+                </div>
+
+                <div class="modal-clientes-area-dados-cliente">
+                    <label>CNPJ</label>
+                    <input
+                        type="text"
+                        name="cnpj"
+                        value="<?php echo $dados_empresa['cnpj']; ?>"
+                    />
+                </div>
+
+            </div>
+
+            <div class="modal-clientes-form-row">
+
+                <div class="modal-clientes-area-dados-cliente area-numero">
+                    <label>CEP</label>
+                    <input
+                        type="text"
+                        name="cep"
+                        value="<?php echo $dados_endereco['cep']; ?>"
+                    />
+                </div>
+
+                <div class="modal-clientes-area-dados-cliente">
+                    <label>Endereço</label>
+                    <input
+                        type="text"
+                        name="rua"
+                        value="<?php echo $dados_endereco['rua']; ?>"
+                    />
+                </div>
+
+                <div class="modal-clientes-area-dados-cliente area-numero">
+                    <label>Número</label>
+                    <input
+                        type="text"
+                        name="numero"
+                        value="<?php echo $dados_endereco['numero']; ?>"
+                    />
+                </div>
+
+                <div class="modal-clientes-area-dados-cliente">
+                    <label>Complemento</label>
+                    <input
+                        type="text"
+                        name="complemento"
+                        value="<?php echo $dados_endereco['complemento']; ?>"
+                    />
+                </div>
+
+            </div>
+
+            <div class="modal-clientes-form-row">
+
+                <div class="modal-clientes-area-dados-cliente">
+                    <label>Bairro</label>
+                    <input
+                        type="text"
+                        name="bairro"
+                        value="<?php echo $dados_endereco['bairro']; ?>"
+                    />
+                </div>
+
+                <div class="modal-clientes-area-dados-cliente">
+                    <label>Cidade</label>
+                    <input
+                        type="text"
+                        name="cidade"
+                        value="<?php echo $dados_endereco['cidade']; ?>"
+                    />
+                </div>
+
+                <div class="modal-clientes-area-dados-cliente">
+                    <label>Estado</label>
+                    <input
+                        type="text"
+                        name="estado"
+                        value="<?php echo $dados_endereco['estado']; ?>"
+                    />
+                </div>
+
+                <div class="modal-clientes-area-dados-cliente">
+                    <label>País</label>
+                    <input
+                        type="text"
+                        name="pais"
+                        value="<?php echo $dados_endereco['pais']; ?>"
+                    />
+                </div>
+
+            </div>
+
+            <div class="modal-clientes-secao-label">
+                <i class="fa-solid fa-user"></i>
+                <h3>Dados do Responsável</h3>
+            </div>
+
+            <div class="modal-clientes-form-row">
+
+                <div class="modal-clientes-area-dados-cliente">
+                    <label>Nome do Responsável</label>
+                    <input
+                        type="text"
+                        name="responsavel"
+                        value="<?php echo $dados_empresa['responsavel']; ?>"
+                    />
+                </div>
+
+            </div>
+
+            <div class="modal-clientes-botoes-form">
+                <a href="listar.php" class="modal-clientes-btn-cancelar">
+                    CANCELAR
+                </a>
+
+                <button type="submit" class="modal-clientes-btn-salvar">
+                    SALVAR
+                </button>
+            </div>
+
+        </div>
+
     </form>
 </body>
 </html>
