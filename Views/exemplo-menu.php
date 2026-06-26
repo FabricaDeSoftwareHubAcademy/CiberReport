dbe9840a9d92489f34bd33910ed124c12815bf67
Rafael  Rotela <rjvrafael22@gmail.com>
2026-06-25 22:50:03 -0400

    fix(menu) tamo ai pra concertar as paradas

--- Views/exemplo-menu.php
+++ Views/exemplo-menu.php
@@ -1,3 +1,53 @@
+<?php
+    require "../Model/Database/Empresa.php";
+    require "../Model/Database/Endereco.php";
+
+    $empresa = new Empresa();
+    $endereco = new Endereco();
+
+    $empresa->conectar("clientesbaikal","localhost","root","");
+    $endereco->conectar("clientesbaikal","localhost","root","");
+
+    if(isset($_POST['nome_fantasia']))
+    {
+        $nome_fantasia = addslashes($_POST['nome_fantasia']);
+        $razao_social = addslashes($_POST['razao_social']);
+        $telefone = addslashes($_POST['telefone']);
+        $email_contato = addslashes($_POST['email_contato']);
+        $cnpj = addslashes($_POST['cnpj']);
+        $responsavel = addslashes($_POST['responsavel']);
+
+        $cep = addslashes($_POST['cep']);
+        $rua = addslashes($_POST['rua']);
+        $numero = addslashes($_POST['numero']);
+        $complemento = addslashes($_POST['complemento']);
+        $bairro = addslashes($_POST['bairro']);
+        $cidade = addslashes($_POST['cidade']);
+        $estado = addslashes($_POST['estado']);
+        $pais = addslashes($_POST['pais']);
+
+        if(!empty($nome_fantasia) && !empty($razao_social) && !empty($telefone) && !empty($email_contato) && !empty($cnpj) && !empty($responsavel) && !empty($cep) && !empty($rua) && !empty($numero) && !empty($bairro) && !empty($cidade) && !empty($estado))
+        {
+        
+            $id_endereco_novo = $endereco->cadastrarEndereco($cep,$rua,$numero,$complemento,$bairro,$cidade,$estado,$pais);
+
+          
+            if($empresa->cadastrarEmpresa($id_endereco_novo,$nome_fantasia,$razao_social,$cnpj,$email_contato,$telefone,$responsavel))
+            {
+                header("location:listar.php");
+            }
+            else
+            {
+                echo "Empresa já cadastrada!";
+            }
+        }
+        else
+        {
+            echo "Preencha todos os campos!";
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
