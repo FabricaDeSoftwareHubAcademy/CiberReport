bae6dd0778086c8fb0b75dc0088ca45697563437
Fabio Elie <fabioelie@outlook.com>
2026-06-26 01:03:39 -0400

    refactor: Limpando arquivos em branco

--- Views/exemplo-menu.php
+++ Views/exemplo-menu.php
@@ -1,55 +1,49 @@
 <?php
-    require "../Model/Database/Empresa.php";
-    require "../Model/Database/Endereco.php";
+require "../Model/Database/Empresa.php";
+require "../Model/Database/Endereco.php";
 
-    $empresa = new Empresa();
-    $endereco = new Endereco();
+$empresa = new Empresa();
+$endereco = new Endereco();
 
-    $empresa->conectar("clientesbaikal","localhost","root","");
-    $endereco->conectar("clientesbaikal","localhost","root","");
+$empresa->conectar("clientesbaikal", "localhost", "root", "");
+$endereco->conectar("clientesbaikal", "localhost", "root", "");
 
-    if(isset($_POST['nome_fantasia']))
-    {
-        $nome_fantasia = addslashes($_POST['nome_fantasia']);
-        $razao_social = addslashes($_POST['razao_social']);
-        $telefone = addslashes($_POST['telefone']);
-        $email_contato = addslashes($_POST['email_contato']);
-        $cnpj = addslashes($_POST['cnpj']);
-        $responsavel = addslashes($_POST['responsavel']);
+if (isset($_POST['nome_fantasia'])) {
+    $nome_fantasia = addslashes($_POST['nome_fantasia']);
+    $razao_social = addslashes($_POST['razao_social']);
+    $telefone = addslashes($_POST['telefone']);
+    $email_contato = addslashes($_POST['email_contato']);
+    $cnpj = addslashes($_POST['cnpj']);
+    $responsavel = addslashes($_POST['responsavel']);
 
-        $cep = addslashes($_POST['cep']);
-        $rua = addslashes($_POST['rua']);
-        $numero = addslashes($_POST['numero']);
-        $complemento = addslashes($_POST['complemento']);
-        $bairro = addslashes($_POST['bairro']);
-        $cidade = addslashes($_POST['cidade']);
-        $estado = addslashes($_POST['estado']);
-        $pais = addslashes($_POST['pais']);
+    $cep = addslashes($_POST['cep']);
+    $rua = addslashes($_POST['rua']);
+    $numero = addslashes($_POST['numero']);
+    $complemento = addslashes($_POST['complemento']);
+    $bairro = addslashes($_POST['bairro']);
+    $cidade = addslashes($_POST['cidade']);
+    $estado = addslashes($_POST['estado']);
+    $pais = addslashes($_POST['pais']);
 
-        if(!empty($nome_fantasia) && !empty($razao_social) && !empty($telefone) && !empty($email_contato) && !empty($cnpj) && !empty($responsavel) && !empty($cep) && !empty($rua) && !empty($numero) && !empty($bairro) && !empty($cidade) && !empty($estado))
-        {
-        
-            $id_endereco_novo = $endereco->cadastrarEndereco($cep,$rua,$numero,$complemento,$bairro,$cidade,$estado,$pais);
+    if (!empty($nome_fantasia) && !empty($razao_social) && !empty($telefone) && !empty($email_contato) && !empty($cnpj) && !empty($responsavel) && !empty($cep) && !empty($rua) && !empty($numero) && !empty($bairro) && !empty($cidade) && !empty($estado)) {
 
-          
-            if($empresa->cadastrarEmpresa($id_endereco_novo,$nome_fantasia,$razao_social,$cnpj,$email_contato,$telefone,$responsavel))
-            {
-                header("location:listar.php");
-            }
-            else
-            {
-                echo "Empresa já cadastrada!";
-            }
+        $id_endereco_novo = $endereco->cadastrarEndereco($cep, $rua, $numero, $complemento, $bairro, $cidade, $estado, $pais);
+
+
+        if ($empresa->cadastrarEmpresa($id_endereco_novo, $nome_fantasia, $razao_social, $cnpj, $email_contato, $telefone, $responsavel)) {
+            header("location:listar.php");
+        } else {
+            echo "Empresa já cadastrada!";
         }
-        else
-        {
-            echo "Preencha todos os campos!";
-        }
+    } else {
+        echo "Preencha todos os campos!";
     }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -57,11 +51,13 @@
     <link rel="stylesheet" href="../Assets/CSS/style.css" />
     <title>CADASTRO Empresa</title>
 </head>
+
 <body>
-    <?php include_once 'menu-lateral.php'; ?> <!-- o menu lateral vem aqui -->
-    <div>
-        <?php include_once 'menu-superior.php'; ?> <!-- O menu lateral separa posto dentro de uma div -->
-        <main class="main-content">
+    <?php include_once 'menu-lateral.php'; ?>
+    <div class="main-content">
+        <?php $tituloPagina = 'Mude Aqui o Título da Página';
+        include_once 'menu-superior.php'; ?>
+        <main>
             <h2 class="titulo-pagina">CADASTRO DE EMPRESA</h2>
             <form method="post">
                 <input type="text" name="nome_fantasia" placeholder="Nome da Empresa">
@@ -83,4 +79,5 @@
         </main>
     </div>
 </body>
+
 </html>
