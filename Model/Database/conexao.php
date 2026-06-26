d0b0d134f7dc6bd2152e7d6ce60ea19e12865dbe
Fabio Elie <fabioelie@outlook.com>
2026-06-26 11:50:09 -0400

    fix: Corrige conexao.php duplicado

--- Model/Database/conexao.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $nome_banco = "clientesbaikal";
-    $host = "localhost";
-    $usuario_bd = "root";
-    $senha_bd = "";
-?>
--- Model/conexao.php
+++ Model/conexao.php
@@ -2,11 +2,10 @@
 $host  = "localhost";
 $user  = "root";
 $pass  = "";
-$banco = "crud544";
+$banco = "cyber_report";
 
 $conexao = new mysqli($host, $user, $pass, $banco);
 
 if ($conexao->connect_error) {
     die("Erro na conexão: " . $conexao->connect_error);
 }
-?>
