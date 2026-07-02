157e8f96b924c28c5bba0433cb856d7195f55d10
Matteus Kill <matteuskill@gmail.com>
2026-07-02 02:29:00 -0400

    feat(projeto): adiciona funcao de listar dados

--- Model/Database/Projeto.php
+++ Model/Database/Projeto.php
@@ -8,4 +8,10 @@ class Projeto{
         $this->pdo = new PDO("mysql:host=$host;dbname=$nome_banco", $usuario, $senha);
     }
 
+    public function listarDados()
+    {
+        $sql = $this->pdo->prepare("SELECT projeto.*, empresa.nome_fantasia FROM projeto INNER JOIN empresa ON projeto.empresa_id = empresa.id ORDER BY projeto.nome");
+        $sql->execute();
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
