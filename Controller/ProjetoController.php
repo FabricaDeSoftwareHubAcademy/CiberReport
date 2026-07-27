da0e4b4dc14337b860388d9dc031963af1168f5d
Fabio Elie <fabioelie@outlook.com>
2026-07-27 09:38:18 -0400

    fix(Projeto): Corrige caminho

--- Controller/ProjetoController.php
+++ Controller/ProjetoController.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . "/../bootstrap.php";
-require_once __DIR__ . "/../Model/Database/Projeto.php";
+require_once __DIR__ . "/../Model/Projeto.php";
 
 
 class ProjetoController
