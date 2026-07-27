99b2a5b1d63494c3515c892f31c2864c49981150
Fabio Elie <fabioelie@outlook.com>
2026-07-27 09:41:05 -0400

    fix(Projeto): Corrige controller

--- Controller/ProjetoController.php
+++ Controller/ProjetoController.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/../bootstrap.php";
+require_once __DIR__ . "/../Model/conexao.php";
 require_once __DIR__ . "/../Model/Projeto.php";
 
 
@@ -9,13 +9,8 @@ class ProjetoController
 
     public function __construct()
     {
-        $this->projeto = new Projeto();
-        $this->projeto->conectar(
-            $_ENV['DB_NAME'],
-            $_ENV['DB_HOST'],
-            $_ENV['DB_USER'],
-            $_ENV['DB_PASS'],
-        );
+        global $conexao;
+        $this->projeto = new Projeto($conexao);
     }
 
     public function listar()
