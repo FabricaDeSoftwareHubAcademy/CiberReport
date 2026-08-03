7d08fced2c54870ef61cc9a03d3e0b8fb4e345f3
Rafael-rotela <rjvrafael22@gmail.com>
2026-08-03 09:00:11 -0400

    subindo as rotas

--- app/routes/main.php
+++ app/routes/main.php
@@ -2,16 +2,8 @@
 
 use http\Route;
 
-// Autenticação
 Route::GET('/', 'AuthController@index');
 Route::POST('/', 'AuthController@index');
-Route::GET('/login', 'AuthController@index');
-Route::POST('/login', 'AuthController@index');
 
-// Tipos de pentest
-Route::GET('/gerenciar-pentest', 'TipoPentestController@index');
-Route::POST('/gerenciar-pentest', 'TipoPentestController@index');
-
-// Compatibilidade temporária com a URL antiga
-Route::GET('/GerenciarPentest', 'TipoPentestController@index');
-Route::POST('/GerenciarPentest', 'TipoPentestController@index');
+Route::GET('/gerenciar-pentest','TipoPentestController@index');
+Route::POST('/gerenciar-pentest','TipoPentestController@index');
