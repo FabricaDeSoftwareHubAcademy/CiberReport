02f48e6fa751d309ba059e216e7cc25991f401e3
Caio Farias <caiovv1@outlook.com>
2026-08-10 00:24:09 -0400

    feat: arquivos mail e mailer.php adiiconados para configuração

---
+++ config/mail.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host'       => 'smtp-mail.outlook.com',
+    'port'       => 587,
+    'username'   => 'caiovv1@outlook.com',
+    'password'   => '',
+    'encryption' => 'tls',
+    'from_email' => 'senac@admin.com.br',
+    'from_name'  => 'CiberReport'
+];
